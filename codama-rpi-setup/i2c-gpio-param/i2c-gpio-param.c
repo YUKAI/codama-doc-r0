3c9d16e56675d64cea4ff14ab8a0ae63aad48ae5
YuheiOKAWA <tochiro.srchack@gmail.com>
2019-08-22 14:51:47 +0900

    Add support kernel 4.19

---
+++ codama-rpi-setup/i2c-gpio-param/i2c-gpio-param.c
@@ -0,0 +1,248 @@
+#include <linux/kernel.h>
+#include <linux/platform_device.h>
+#include <linux/module.h>
+#include <linux/i2c-gpio.h>
+#include <linux/init.h>
+#include <linux/version.h>
+
+
+#define MAX_BUSES 8
+#define MODNAME "i2c-gpio-param"
+
+int pin_blacklist[]={5,6};
+
+struct bus {
+    int id;
+    struct platform_device *pdev;
+};
+
+struct bus busses[MAX_BUSES];
+static int n_busses;
+
+static int addbus(unsigned int id, struct i2c_gpio_platform_data pdata, int test);
+static void removebus(unsigned int i);
+
+static int busid=7;
+static int sda=0;
+static int scl=1;
+static int udelay=0;
+static int timeout=0;
+static int sda_od=0;
+static int scl_od=0;
+static int scl_oo=0;
+
+module_param(busid, int, S_IRUSR);
+MODULE_PARM_DESC(busid, "I2C bus id");
+
+module_param(sda, int, S_IRUSR);
+MODULE_PARM_DESC(sda, "SDA pin");
+
+module_param(scl, int, S_IRUSR);
+MODULE_PARM_DESC(scl, "SCL pin");
+
+module_param(udelay, int, S_IRUSR);
+MODULE_PARM_DESC(udelay, "SCL frequency is (500 / udelay) kHz.");
+
+module_param(timeout, int, S_IRUSR);
+MODULE_PARM_DESC(timeout, "Clock stretching timeout in jiffies.");
+
+module_param(sda_od, int, S_IRUSR);
+MODULE_PARM_DESC(sda_od, "SDA is configured as open drain.");
+
+module_param(scl_od, int, S_IRUSR);
+MODULE_PARM_DESC(scl_od, "SCL is configured as open drain.");
+
+module_param(scl_oo, int, S_IRUSR);
+MODULE_PARM_DESC(scl_oo, "SCL output drivers cannot be turned off (no clock stretching).");
+
+static ssize_t add_bus_store(struct class *class,
+                struct class_attribute *attr,
+                const char *buf, size_t count)
+{
+    struct i2c_gpio_platform_data pdata={};
+    unsigned int id, ret, sdaod, sclod, scloo;
+
+    ret=sscanf(buf, "%u %u %u %u %u %u %u %u",
+               &id, &pdata.sda_pin, &pdata.scl_pin, &pdata.udelay, &pdata.timeout,
+               &sdaod, &sclod, &scloo);
+    if(ret<3) {
+        printk(KERN_INFO MODNAME " add_bus: Missing or wrong required parameters (busid, sda, scl).\n");
+        return -EINVAL;
+    }
+    if(ret>5) {
+        pdata.sda_is_open_drain=sdaod;
+    }
+    if(ret>6) {
+        pdata.scl_is_open_drain=sclod;
+    }
+    if(ret>7) {
+        pdata.scl_is_output_only=scloo;
+    }
+    printk(KERN_INFO "id=%u, sda=%u, scl=%u, udelay=%u, timeout=%u, sda_od=%u, scl_od=%u, scl_oo=%u\n",
+           id, pdata.sda_pin, pdata.scl_pin, pdata.udelay, pdata.timeout, pdata.sda_is_open_drain,
+           pdata.scl_is_open_drain, pdata.scl_is_output_only);
+
+    ret=addbus(id, pdata, 1);
+    if(ret) {
+       return ret;
+    }
+
+    return count;
+}
+
+static ssize_t remove_bus_store(struct class *class,
+                struct class_attribute *attr,
+                const char *buf, size_t count)
+{
+    unsigned int id,i;
+
+    if(sscanf(buf, "%u", &id)<1) {
+        printk(KERN_INFO MODNAME " remove_bus: Missing parameter.\n");
+        return -EINVAL;
+    }
+
+    for(i=0; i<n_busses; i++) {
+        if(busses[i].id==id) {
+            removebus(i);
+            if(i+1<n_busses)
+                busses[i]=busses[n_busses-1];
+
+            n_busses--;
+            return count;
+        }
+    }
+
+    return -ENOENT;
+}
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 10, 0)
+static CLASS_ATTR_WO(add_bus);
+static CLASS_ATTR_WO(remove_bus);
+static struct attribute *i2c_gpio_param_class_attrs[] = {
+		&class_attr_add_bus.attr,
+		&class_attr_remove_bus.attr,
+		NULL,
+};
+ATTRIBUTE_GROUPS(i2c_gpio_param_class);
+#else
+static struct class_attribute i2c_gpio_param_class_attrs[] = {
+		__ATTR(add_bus, 0200, NULL, add_bus_store),
+		__ATTR(remove_bus, 0200, NULL, remove_bus_store),
+		__ATTR_NULL,
+};
+#endif
+
+static struct class i2c_gpio_param_class = {
+    .name =     "i2c-gpio",
+    .owner =    THIS_MODULE,
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 10, 0)
+    .class_groups = i2c_gpio_param_class_groups,
+#else
+    .class_attrs =  i2c_gpio_param_class_attrs,
+#endif
+};
+
+static void removebus(unsigned int i) {
+    platform_device_unregister(busses[i].pdev);
+}
+
+static int addbus(unsigned int id, struct i2c_gpio_platform_data pdata, int test)
+{
+    int ret;
+    unsigned int i;
+    struct platform_device *pdev;
+
+    if(n_busses>=MAX_BUSES) {
+        return -ENOMEM;
+    }
+
+    for(i=0; i<n_busses; i++) {
+        if(busses[i].id==id)
+            return -EEXIST;
+    }
+
+    for(i=0; i<sizeof(pin_blacklist)/sizeof(int); i++) {
+        if (pdata.sda_pin==pin_blacklist[i] ||
+            pdata.scl_pin==pin_blacklist[i]) {
+            printk(KERN_ERR MODNAME ": Requested pin (%d) is blacklisted.\n",
+                   pin_blacklist[i]);
+            return -EINVAL;
+        }
+    }
+
+    pdev = platform_device_alloc("i2c-gpio", id);
+    if (!pdev) {
+         return -ENOMEM;
+    }
+
+    ret = platform_device_add_data(pdev, &pdata, sizeof(pdata));
+    if(ret) {
+        platform_device_put(pdev);
+        return ret;
+    }
+
+    ret = platform_device_add(pdev);
+    if(ret) {
+        return ret;
+    }
+    // platform_device_add won't return error code if GPIO resources are not available,
+    // for example. A workaround is to check if drvdata is set after this function
+    // as this is the last thing i2c_gpio_probe.
+    // Unfortunately this won't work when adding bus from init code as the actual
+    // probe will run only after we finish are initialization.
+
+    if(test && platform_get_drvdata(pdev)==NULL) {
+        printk(KERN_ERR MODNAME ": Got error when registering the bus.\n");
+        platform_device_unregister(pdev);
+        return -EEXIST;
+    }
+
+    busses[n_busses++]=(struct bus){.id=id, .pdev=pdev};
+
+    return 0;
+}
+
+static int __init i2c_gpio_param_init(void)
+{
+    struct i2c_gpio_platform_data pdata;
+    int ret;
+
+    pdata.sda_pin = sda;
+    pdata.scl_pin = scl;
+    pdata.udelay = udelay;
+    pdata.timeout = timeout;
+    pdata.sda_is_open_drain = sda_od;
+    pdata.scl_is_open_drain = scl_od;
+    pdata.scl_is_output_only = scl_oo;
+
+    ret=addbus(busid, pdata, 0);
+    if(ret) {
+        return ret;
+    }
+
+    ret = class_register(&i2c_gpio_param_class);
+    if (ret < 0)
+        return ret;
+
+
+    return 0;
+}
+
+static void __exit i2c_gpio_param_exit(void)
+{
+    int i;
+
+    class_unregister(&i2c_gpio_param_class);
+    for(i=0; i<n_busses; i++) {
+        removebus(i);
+    }
+}
+
+module_init(i2c_gpio_param_init);
+module_exit(i2c_gpio_param_exit);
+
+MODULE_LICENSE("GPL v2");
+MODULE_AUTHOR("XMOS");
+MODULE_DESCRIPTION("I2C-GPIO Driver");
+MODULE_VERSION("1.0");
---
+++ codama-rpi-setup/setup.sh
@@ -0,0 +1,103 @@
+#!/usr/bin/env bash
+pushd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null
+RPI_SETUP_DIR="$( pwd )"
+
+# Disable the built-in audio output so there is only one audio
+# device in the system
+sudo sed -i -e 's/^dtparam=audio=on/#dtparam=audio=on/' /boot/config.txt
+
+# Enable the i2s device tree
+sudo sed -i -e 's/#dtparam=i2s=on/dtparam=i2s=on/' /boot/config.txt
+
+echo "Installing Raspberry Pi kernel headers"
+sudo apt-get install -y raspberrypi-kernel-headers
+
+# Build loader and insert it into the kernel
+if [ "`uname -m`" = "armv6l" ] ; then
+    sed -i 's/3f203000\.i2s/20203000\.i2s/' loader/loader.c
+else
+    sed -i 's/20203000\.i2s/3f203000\.i2s/' loader/loader.c
+fi
+pushd $RPI_SETUP_DIR/loader > /dev/null
+make i2s_slave
+popd > /dev/null
+sudo cp $RPI_SETUP_DIR/loader/loader.ko /lib/modules/`uname -r`/kernel/sound/drivers/
+sudo depmod -ae
+if ! grep -q "loader" /etc/modules-load.d/modules.conf; then
+    sudo sed -i -e '$ a loader' /etc/modules-load.d/modules.conf
+fi
+
+# Move existing files to back up
+if [ -e ~/.asoundrc ] ; then
+    chmod a+w ~/.asoundrc
+    cp ~/.asoundrc ~/.asoundrc.bak
+    sudo cp /root/.asoundrc /root/.asoundrc.bak
+fi
+if [ -e /usr/share/alsa/pulse-alsa.conf ] ; then
+    sudo mv /usr/share/alsa/pulse-alsa.conf  /usr/share/alsa/pulse-alsa.conf.bak
+    sudo mv ~/.config/lxpanel/LXDE-pi/panels/panel ~/.config/lxpanel/LXDE-pi/panels/panel.bak
+fi
+if [ -e ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-mixer.xml ] ; then
+  cp ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-mixer.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-mixer.xml.bak
+fi
+
+# Check args for asoundrc selection. Default to VF Stereo.
+cp $RPI_SETUP_DIR/resources/asoundrc_vf ~/.asoundrc
+sudo cp $RPI_SETUP_DIR/resources/asoundrc_vf /root/.asoundrc
+
+cp $RPI_SETUP_DIR/resources/panel ~/.config/lxpanel/LXDE-pi/panels/panel
+mkdir -p ~/.config/xfce4/xfconf/xfce-perchannel-xml/
+cp $RPI_SETUP_DIR/resources/xfce4-mixer.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-mixer.xml
+chmod 700 ~/.config/xfce4/xfconf/xfce-perchannel-xml
+
+# Make the asoundrc file read-only otherwise lxpanel rewrites it
+# as it doesn't support anything but a hardware type device
+chmod a-w ~/.asoundrc
+sudo chmod a-w /root/.asoundrc
+
+
+# Apply changes
+sudo /etc/init.d/alsa-utils restart
+
+
+# Configure the I2C - disable the default built-in driver
+sudo sed -i -e 's/#\?dtparam=i2c_arm=on/dtparam=i2c_arm=off/' /boot/config.txt
+if ! grep -q "i2c-bcm2708" /etc/modules-load.d/modules.conf; then
+  sudo sh -c 'echo i2c-bcm2708 >> /etc/modules-load.d/modules.conf'
+fi
+if ! grep -q "i2c-dev" /etc/modules-load.d/modules.conf; then
+  sudo sh -c 'echo i2c-dev >> /etc/modules-load.d/modules.conf'
+fi
+if ! grep -q "options i2c-bcm2708 combined=1" /etc/modprobe.d/i2c.conf; then
+  sudo sh -c 'echo "options i2c-bcm2708 combined=1" >> /etc/modprobe.d/i2c.conf'
+fi
+
+# Build a new I2C driver
+if [ "`uname -r | cut -d. -f1-2`" != "4.19" ] ; then
+    pushd $RPI_SETUP_DIR/i2c-gpio-param > /dev/null
+    make || exit $?
+    popd > /dev/null
+    sudo cp $RPI_SETUP_DIR/i2c-gpio-param/i2c-gpio-param.ko /lib/modules/`uname -r`/kernel/drivers/i2c/
+    sudo depmod -ae
+    if ! grep -q "i2c-gpio-param" /etc/modules-load.d/modules.conf; then
+        sudo sed -i -e '$ a i2c-gpio-param' /etc/modules-load.d/modules.conf
+    fi
+    if ! grep -q "options i2c-gpio-param busid=1 sda=2 scl=3 udelay=5 timeout=100 sda_od=0 scl_od=0 scl_oo=0" /etc/modprobe.d/i2c.conf; then
+        sudo sed -i -e '$ a options i2c-gpio-param busid=3 sda=2 scl=3 udelay=5 timeout=100 sda_od=0 scl_od=0 scl_oo=0' /etc/modprobe.d/i2c.conf
+    fi
+else
+    if ! grep -q "dtoverlay=i2c-gpio,bus=3,i2c_gpio_sda=2,i2c_gpio_scl=3,i2c_gpio_delay_us=5,timeout-ms=100" /boot/config.txt; then
+        sudo sed -i -e '$ a dtoverlay=i2c-gpio,bus=3,i2c_gpio_sda=2,i2c_gpio_scl=3,i2c_gpio_delay_us=5,timeout-ms=100' /boot/config.txt
+    fi
+fi
+
+
+if ! grep -q "# Run Alsa at startup so that alsamixer configures" /etc/rc.local; then
+    sudo sed -i -e '$i \# Run Alsa at startup so that alsamixer configures' /etc/rc.local
+    sudo sed -i -e '$i \arecord -d 1 > /dev/null 2>&1' /etc/rc.local
+    sudo sed -i -e '$i \aplay dummy > /dev/null 2>&1' /etc/rc.local
+fi
+
+echo "To enable I2S and I2C, this Raspberry Pi must be rebooted."
+
+popd > /dev/null
