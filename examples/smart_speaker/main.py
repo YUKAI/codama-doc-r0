Fixed input overflow error

examples/smart_speaker/main.py
@@ -1,6 +1,7 @@
 import pvporcupine
 import queue, sys
 import sounddevice as sd
+import numpy as np
 import audio
 import api
 import os
@@ -8,18 +9,25 @@ from dotenv import load_dotenv
 
 load_dotenv('../.env') 
 
-REC_FILE = "../sound/rec.wav"
-OUTPUT_FILE = "../sound/output.wav"
-
 AUDIO_DEVICE_NUM = 8
-DOWN_SAMPLE = 1
+
+# 録音の設定
 SAMPLE_RATE = 16000
 CHANNELS = 1
+
+REC_FILE = "../sound/rec.wav"
+OUTPUT_FILE = "../sound/output.wav"
 
 # ChatGPTのキャラクター設定
 CHAT_CHARACTER = '''あなたは英会話教室の教師です。
 全て英語の歌にして返答します。必ず3行で返答します。
 簡単な英単語だけを使ってください。'''
+
+sd.default.device = AUDIO_DEVICE_NUM
+
+codama = audio.Audio(SAMPLE_RATE, CHANNELS)
+openai = api.OpenAI()
+google = api.Google()
 
 # porcupineの設定
 porcupine = pvporcupine.create(
@@ -27,12 +35,8 @@ porcupine = pvporcupine.create(
   keyword_paths=["../kodama_ja_raspberry-pi_v2_2_0.ppn"],
   model_path="../porcupine_params_ja.pv"
 )
-q = queue.Queue()
 
-codama = audio.Audio(SAMPLE_RATE, CHANNELS)
-openai = api.OpenAI()
-google = api.Google()
-sd.default.device = AUDIO_DEVICE_NUM
+q = queue.Queue()
 
 def recordCallback(indata, frames, time, status):
     if status:
@@ -44,40 +48,37 @@ def run():
         stream = sd.InputStream(
             samplerate=SAMPLE_RATE,
             dtype="int16",
-            blocksize=512,
+            blocksize=porcupine.frame_length,
             channels=CHANNELS,
             callback=recordCallback,
         )
         stream.start()
-
         print("Start")
-        cont = True
 
-        while cont:
-            while q.empty():
-                pass
-            data = q.get(block=False)
-            data = data[::DOWN_SAMPLE, 0]
+        while True:
+            if not q.empty():
+                data = q.get(block=False)
+                data = np.reshape(data, [data.shape[0]])
+                
+                keyword_index = porcupine.process(data)
+                
+                # "こだま"を検知したら
+                if keyword_index == 0:
+                    print("Detected: こだま")
+                    stream.stop()
+                    stream.close()
 
-            keyword_index = porcupine.process(data)
-
-            # "こだま"を検知したら
-            if keyword_index == 0:
-                print("Detected: こだま")
-                cont = False
-                stream.stop()
-                stream.close()
-
-                # 3秒間録音
-                codama.record(REC_FILE, 3)
-                # Whisper APIで音声データをテキストに変換
-                input_text = openai.whisper(REC_FILE)
-                # ChatGPT APIで返答を生成
-                reply_text = openai.chatgpt(input_text, CHAT_CHARACTER)
-                # Google Cloud Text-to-Speech APIで音声合成
-                google.synthesize_text(reply_text, OUTPUT_FILE)
-                # スピーカーで再生
-                codama.play(OUTPUT_FILE)
+                    # 3秒間録音
+                    codama.record(REC_FILE, 3)
+                    # Whisper APIで音声データをテキストに変換
+                    input_text = openai.whisper(REC_FILE)
+                    # ChatGPT APIで返答を生成
+                    reply_text = openai.chatgpt(input_text, CHAT_CHARACTER)
+                    # Google Cloud Text-to-Speech APIで音声合成
+                    google.synthesize_text(reply_text, OUTPUT_FILE)
+                    # スピーカーで再生
+                    codama.play(OUTPUT_FILE)
+                    break
 
     except KeyboardInterrupt:
         pass

examples/wakeup_word.py
@@ -1,5 +1,6 @@
 import pvporcupine
 import queue, sys
+import numpy as np
 import sounddevice as sd
 import os
 from dotenv import load_dotenv
@@ -7,18 +8,21 @@ from dotenv import load_dotenv
 load_dotenv('.env') 
 
 AUDIO_DEVICE_NUM = 8
-DOWN_SAMPLE = 1
+
+# 録音の設定
 SAMPLE_RATE = 16000
 CHANNELS = 1
 
+sd.default.device = AUDIO_DEVICE_NUM
+
+# porcupineの設定
 porcupine = pvporcupine.create(
   access_key=os.environ.get("ACCESS_KEY"),
   keyword_paths=["kodama_ja_raspberry-pi_v2_2_0.ppn"],
   model_path="porcupine_params_ja.pv"
 )
-q = queue.Queue()
 
-sd.default.device = AUDIO_DEVICE_NUM
+q = queue.Queue()
 
 def recordCallback(indata, frames, time, status):
     if status:
@@ -30,26 +34,23 @@ def run():
         stream = sd.InputStream(
             samplerate=SAMPLE_RATE,
             dtype="int16",
-            blocksize=512,
+            blocksize=porcupine.frame_length,
             channels=CHANNELS,
             callback=recordCallback,
         )
         stream.start()
-
         print("Start")
-        cont = True
 
-        while cont:
-            while q.empty():
-                pass
-            data = q.get(block=False)
-            data = data[::DOWN_SAMPLE, 0]
-
-            keyword_index = porcupine.process(data)
-
-            # "こだま"を検知したら
-            if keyword_index == 0:
-                print("Detected: こだま")
+        while True:
+            if not q.empty():
+                data = q.get(block=False)
+                data = np.reshape(data, [data.shape[0]])
+                
+                keyword_index = porcupine.process(data)
+                
+                # "こだま"を検知したら
+                if keyword_index == 0:
+                    print("Detected: こだま")
 
     except KeyboardInterrupt:
         pass
@@ -57,6 +58,7 @@ def run():
         sd.stop()
         while not q.empty():
             q.get(block=False)
+
 
 if __name__ == "__main__":
     run()